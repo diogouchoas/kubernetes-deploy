e2d81074f6cc8254c130aa583ca2e220fb98ccaa
Daniel Turner <daniel.turner@shopify.com>
2018-06-05 12:10:05 -0500

    Add the file

---
+++ lib/kubernetes-deploy/kubernetes_resource/customresourcedefinition.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+module KubernetesDeploy
+  class CustomResourceDefinition < KubernetesResource
+    TIMEOUT = 10.seconds
+
+    def deploy_succeeded?
+      exists?
+    end
+
+    def deploy_failed?
+      false
+    end
+
+    def timeout_message
+      UNUSUAL_FAILURE_MESSAGE
+    end
+  end
+end
